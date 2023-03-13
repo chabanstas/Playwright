abb57bd0653aed99c365a284f82a2341d4a2f00c
Stas Chaban <105775681+chabanstas@users.noreply.github.com>
2023-03-13 19:18:10 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,12 +47,6 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
     
   ],
 
